31b180e515778a8725ca1725d3144a3114667ec3
Bernd Wagner <berndwagner@MacBook-Pro-von-Bernd.local>
2025-05-16 18:54:23 +0200

    Basic navigation and icons.

--- frontend/app/src/main/java/com/opensock/android/MainActivity.kt
+++ frontend/app/src/main/java/com/opensock/android/MainActivity.kt
@@ -11,26 +11,53 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Button
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.opensock.android.ui.theme.OpenSockAndroidAppTheme
 import androidx.core.net.toUri
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+private const val TAG = "OpenSock"
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
@@ -58,10 +85,7 @@ class MainActivity : ComponentActivity() {
             OpenSockAndroidAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     if (viewModel.isPermissionGranted.value) {
-                        Greeting(
-                            name = "Android",
-                            modifier = Modifier.padding(innerPadding)
-                        )
+                        CameraPreviewScreen()
                     } else {
                         CameraPermissionRequiredScreen(context = this)
                     }
@@ -71,22 +95,132 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private suspend fun Context.getCameraProvider(): ProcessCameraProvider =
+    suspendCoroutine { continuation ->
+        ProcessCameraProvider.getInstance(this).also { cameraProvider ->
+            cameraProvider.addListener({
+                continuation.resume(cameraProvider.get())
+            }, ContextCompat.getMainExecutor(this))
+        }
+    }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    OpenSockAndroidAppTheme {
-        Greeting("Android")
+fun CameraPreviewScreen() {
+    val lensFacing = CameraSelector.LENS_FACING_BACK
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+    val preview = Preview.Builder().build()
+    val previewView = remember {
+        PreviewView(context)
+    }
+    val cameraxSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+    LaunchedEffect(lensFacing) {
+        val cameraProvider = context.getCameraProvider()
+        cameraProvider.unbindAll()
+        cameraProvider.bindToLifecycle(lifecycleOwner, cameraxSelector, preview)
+        preview.surfaceProvider = previewView.surfaceProvider
+    }
+    Box(modifier = Modifier.fillMaxSize()) {
+        AndroidView(factory = { previewView }, modifier = Modifier.fillMaxSize())
+
+        Box(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 36.dp)
+        ) {
+            // ShutterButton()
+            // RejectAcceptButtons()
+            UndoButton()
+        }
 
     }
 }
+
+@Composable
+fun ShutterButton() {
+    Box(
+        modifier = Modifier
+            .size(120.dp)
+            .clip(CircleShape)
+            .clickable(
+                onClick = {
+
+                }
+            ),
+        contentAlignment = Alignment.BottomCenter
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.camera_shutter),
+            contentDescription = null,
+            colorFilter = ColorFilter.tint(Color.White),
+            modifier = Modifier.size(120.dp)
+        )
+    }
+}
+
+@Composable
+fun RejectAcceptButtons(
+    onReject: () -> Unit = {},
+    onAccept: () -> Unit = {}
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(32.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .wrapContentWidth()
+            .height(120.dp)
+    ) {
+        ActionCircleButton(
+            iconRes = R.drawable.close,
+            contentDescription = "Reject",
+            tint = Color.Red,
+            onClick = onReject
+        )
+        ActionCircleButton(
+            iconRes = R.drawable.check,
+            contentDescription = "Accept",
+            tint = Color(0xFF00C853), // green
+            onClick = onAccept
+        )
+    }
+}
+
+@Composable
+fun UndoButton(
+    onUndo: () -> Unit = {}
+) {
+    ActionCircleButton(
+        iconRes = R.drawable.arrow_back,
+        contentDescription = null,
+        tint = Color.Black,
+        onClick = onUndo
+    )
+}
+
+@Composable
+fun ActionCircleButton(
+    iconRes: Int,
+    contentDescription: String?,
+    tint: Color,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .size(96.dp)
+            .clip(CircleShape)
+            .background(Color.White)
+            .clickable(onClick = onClick),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = iconRes),
+            contentDescription = contentDescription,
+            colorFilter = ColorFilter.tint(tint),
+            modifier = Modifier.size(56.dp)
+        )
+    }
+}
+
 
 @Composable
 fun CameraPermissionRequiredScreen(context: Context) {
